77624bf625033207e1d4ceea10d63b1bee38cdec
Rahul Parkar <rahul@parkar.io>
2025-12-24 01:33:40 +0000

    Add Sentry debugger

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,16 @@ fastify.get("/", (_, reply) => {
   reply.send({ hello: "world" });
 });
 
+fastify.get("/debug-sentry", function mainHandler() {
+  // Send a log before throwing the error
+  Sentry.logger.info("User triggered test error", {
+    action: "test_error_endpoint",
+  });
+  // Send a test metric before throwing the error
+  Sentry.metrics.count("test_counter", 1);
+  throw new Error("My first Sentry error!");
+});
+
 // Run the server!
 fastify.listen({ host: "0.0.0.0", port: 3008 }, (err) => {
   if (err) {
