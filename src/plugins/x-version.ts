7dfc1f17cd8a85543ec46845275072d5004c2fd6
Rahul Parkar <rahul@parkar.io>
2025-12-26 19:52:25 +0000

    Add commit sha to responses to prove env var is working

---
+++ src/plugins/x-version.ts
@@ -0,0 +1,12 @@
+import fp from "fastify-plugin";
+
+/**
+ * This plugin adds some utilities to handle http errors
+ *
+ * @see https://github.com/fastify/fastify-sensible
+ */
+export default fp(async (fastify) => {
+  fastify.addHook("onSend", async (_, reply) => {
+    reply.header("X-API-Version", process.env.COMMIT_SHA);
+  });
+});
